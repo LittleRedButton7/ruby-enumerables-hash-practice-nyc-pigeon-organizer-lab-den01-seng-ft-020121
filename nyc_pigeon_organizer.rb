4da62e1cbbc184009e8472a02870a273192e288f
Marissa Nolan <ritz1484@cs.com>
2021-01-21 19:22:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,8 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  #we have a hash of descriptive data per pigeon name.
+  #we want a hash broken down by pigeon name that contains the descriptive data
+  #so, essentially, we have to break apart or abstract that data into a form that we want sorted by name
+  #once we abstract that data, we need to organize by the name being the new key and the hash of attributes being the value 
+  
 end
