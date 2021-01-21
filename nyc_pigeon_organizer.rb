98306c870dee07db31585623ecda5f874adec5cc
Marissa Nolan <ritz1484@cs.com>
2021-01-21 19:47:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,9 +17,8 @@ def nyc_pigeon_organizer(data)
         if !final_list[name][key]
           !final_list[name][key] = []
       end
-      final_list[name][key].push
+      final_list[name][key].push(inner_key.to_s)
     end
-    final_list
     
   end     
 end
