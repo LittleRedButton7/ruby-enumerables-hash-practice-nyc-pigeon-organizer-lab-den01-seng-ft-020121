474fb398cc04acb87cbb9dbe7603bc122a4a2fd7
Marissa Nolan <ritz1484@cs.com>
2021-01-21 20:00:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #we have a hash of descriptive data per pigeon name.
+  #we want a hash broken down by pigeon name that contains the descriptive data
+  #so, essentially, we have to break apart or abstract that data into a form that we want sorted by name
+  #once we abstract that data, we need to organize by the name being the new key and the hash of attributes being the value 
+  
+  final_results = data.each_with_object({}) do |(key, value), final_list|
+
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_list[name]
+          final_list[name] = {}
+        end
+        if !final_list[name][key]
+          !final_list[name][key] = []
+        end
+        final_list[name][key].push(inner_key.to_s)
+      end
+    end
+  end  
 end
